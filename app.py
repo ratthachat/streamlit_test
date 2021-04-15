d397413b3726dca641b6e0bea1a14b6e6ed1f074
Ratthachat (Jung) <56621342+ratthachat@users.noreply.github.com>
2021-04-15 17:53:20 +0700

    Debug with stricter than Openai

--- app.py
+++ app.py
@@ -176,37 +176,37 @@ def safe_conversation_generator(hidden_prompt_en):
     # vs. should be discarded as a false positive
     toxic_threshold = -0.355
 
-    if output_label == "2":
-        # If the model returns "2", return its confidence in 2 or other output-labels
-        logprobs = response["choices"][0]["logprobs"]["top_logprobs"][0]
+#     if output_label == "2":
+#         # If the model returns "2", return its confidence in 2 or other output-labels
+#         logprobs = response["choices"][0]["logprobs"]["top_logprobs"][0]
 
-        # If the model is not sufficiently confident in "2",
-        # choose the most probable of "0" or "1"
-        # Guaranteed to have a confidence for 2 since this was the selected token.
-        if logprobs["2"] < toxic_threshold:
-            logprob_0 = logprobs.get("0", None)
-            logprob_1 = logprobs.get("1", None)
+#         # If the model is not sufficiently confident in "2",
+#         # choose the most probable of "0" or "1"
+#         # Guaranteed to have a confidence for 2 since this was the selected token.
+#         if logprobs["2"] < toxic_threshold:
+#             logprob_0 = logprobs.get("0", None)
+#             logprob_1 = logprobs.get("1", None)
 
-            # If both "0" and "1" have probabilities, set the output label
-            # to whichever is most probable
-            if logprob_0 is not None and logprob_1 is not None:
-                if logprob_0 >= logprob_1:
-                    output_label = "0"
-                else:
-                    output_label = "1"
-            # If only one of them is found, set output label to that one
-            elif logprob_0 is not None:
-                output_label = "0"
-            elif logprob_1 is not None:
-                output_label = "1"
+#             # If both "0" and "1" have probabilities, set the output label
+#             # to whichever is most probable
+#             if logprob_0 is not None and logprob_1 is not None:
+#                 if logprob_0 >= logprob_1:
+#                     output_label = "0"
+#                 else:
+#                     output_label = "1"
+#             # If only one of them is found, set output label to that one
+#             elif logprob_0 is not None:
+#                 output_label = "0"
+#             elif logprob_1 is not None:
+#                 output_label = "1"
 
-            # If neither "0" or "1" are available, stick with "2"
-            # by leaving output_label unchanged.
+#             # If neither "0" or "1" are available, stick with "2"
+#             # by leaving output_label unchanged.
 
-    # if the most probable token is none of "0", "1", or "2"
-    # this should be set as unsafe
-    if output_label not in ["0", "1", "2"]:
-        output_label = "2"
+#     # if the most probable token is none of "0", "1", or "2"
+#     # this should be set as unsafe
+#     if output_label not in ["0", "1", "2"]:
+#         output_label = "2"
 
     if output_label == "2": # Unsafe, change output
         generated_text = "Hmmm... I'm almost speak impolite words due to yours. Please speak again politely."
